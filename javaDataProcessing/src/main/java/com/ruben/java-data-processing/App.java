105836e6840599ef5d06a14fb43b54a776b590b1
Ruben Rodriguez <aki_yoshi@outlook.com>
2020-11-11 20:58:52 +0100

    Rework menu, add functionality

--- javaDataProcessing/src/main/java/com/ruben/java-data-processing/App.java
+++ javaDataProcessing/src/main/java/com/ruben/java-data-processing/App.java
@@ -14,43 +14,50 @@ public class App
 
     public static void main( String[] args )
     {
-        System.out.println("Hello World!");
         Scanner sn = new Scanner(System.in);
         boolean exit = false;
         int option;
 
         while (!exit) {
 
-            System.out.println("1. Func 1");
-            System.out.println("2. Func 2");
-            System.out.println("3. Func 3");
-            System.out.println("4. Exit");
+            System.out.println();
+            System.out.println("\t1 - Get Min and Max Trip prices!");
+            System.out.println("\t2 - Origins and Destination counts!");
+            System.out.println("\t3 - Print all Data");
+            System.out.println("\t4 - Exit\n");
         
             try {
 
                 CSVUtils csvParser = new CSVUtils();
-                System.out.println("Enter a valid option from menu:");
+                System.out.print("\tEnter a valid option from menu: ");
                 option = sn.nextInt();
 
                 switch (option) {
                     case 1:
-                        System.out.println("Func 1!");
-                        List<String[]> test = csvParser.readAllData(CSV_FILE_NAME);
+                        System.out.println("\n\tCalculating Min and Max Trip prices...\n\n");
+                        Double maxPrice = csvParser.getMinMaxPrice(CSV_FILE_NAME);
                         break;
                     case 2:
-                        System.out.println("Func 2!");
+                        csvParser.countByOrigDest(CSV_FILE_NAME);
                         break;
                     case 3:
-                        System.out.println("Func 3!");
+                        System.out.println("\n\tPrinting all data...\n\n");
+                        List<String[]> test = csvParser.readAllData(CSV_FILE_NAME);
+                        for (String[] row : test) { 
+                           for (String cell : row) { 
+                                System.out.print(cell + "\t"); 
+                            } 
+                            System.out.println(); 
+                        } 
                         break;
                     case 4:
                         exit = true;
                         break;
                     default:
-                        System.out.println("Only number in menu range");
+                        System.out.println("\t\nOnly option in menu range accepted!\n");
                 }
             } catch (InputMismatchException e) {
-                System.out.println("Number expected!");
+                System.out.println("\t\nNot a valid option!\n");
                 sn.next();
             }
         }

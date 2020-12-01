4e68b26015062f2d95a469eeb3a5ce17b550adf4
Ruben Rodriguez <aki_yoshi@outlook.com>
2020-12-01 10:46:43 +0100

    Add spark sample

--- sparkDataProcessing/spark.py
+++ sparkDataProcessing/spark.py
@@ -3,9 +3,9 @@ import sys
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as f
 
-sparkSession = SparkSession.builder.appName("example-pyspark-read-and-write").getOrCreate()
+sparkSession = SparkSession.builder.appName("example-pyspark-read").getOrCreate()
 
-df_load = sparkSession.read.csv('hdfs://cluster/user/hdfs/test/example.csv')
+df_load = sparkSession.read.csv('hdfs://hadoop-master/dataProcessing/input/sample.csv')
 
 df_load.show()
 

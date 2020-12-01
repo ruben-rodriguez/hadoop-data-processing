932eecac13067fefbec0982f2115b1a938191c76
Ruben Rodriguez <aki_yoshi@outlook.com>
2020-12-01 12:09:28 +0100

    Add more calculations

--- sparkDataProcessing/spark.py
+++ sparkDataProcessing/spark.py
@@ -3,6 +3,8 @@ import sys
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as f
 
+from pyspark.sql.functions import sum, avg, max, min, mean, count
+
 sparkSession = SparkSession.builder.appName("example-pyspark-read").getOrCreate()
 
 df_load = sparkSession.read.option("header",True).csv('hdfs://hadoop-master:9000/dataProcessing/input/sample.csv')
@@ -18,7 +20,6 @@ df_load.groupBy('destination', 'vehicle_type').count().select('destination', f.c
 df_load.groupBy('origin', 'vehicle_type').count().select('origin', f.col('count').alias('count')).show()
 
 df_load.groupBy('vehicle_class').agg(
-    avg("price").alias("avg_salary"), \
-    sum("price").alias("sum_bonus"), \
-    max("price").alias("max_bonus") \
+    avg("price").alias("avg_price"), \
+    max("price").alias("max_price") \
 )

69d5d82f42a0c564297b72cc61a5a08329b2345a
Ruben Rodriguez <aki_yoshi@outlook.com>
2020-12-01 12:23:07 +0100

    Add reading option to drop malformed records

--- sparkDataProcessing/spark.py
+++ sparkDataProcessing/spark.py
@@ -7,7 +7,7 @@ from pyspark.sql.functions import sum, avg, max, min, mean, count
 
 sparkSession = SparkSession.builder.appName("example-pyspark-read").getOrCreate()
 
-df_load = sparkSession.read.option("header",True).option("mode", "DROPMALFORMED").csv('hdfs://hadoop-master:9000/dataProcessing/input/sample.csv')
+df_load = sparkSession.read.option("header",True).csv('hdfs://hadoop-master:9000/dataProcessing/input/sample.csv')
 
 df_load.show()
 
@@ -22,4 +22,4 @@ df_load.groupBy('origin', 'vehicle_type').count().select('origin', f.col('count'
 df_load.groupBy('vehicle_class').agg(
     avg("price").alias("avg_price"), \
     max("price").alias("max_price") \
-).show(truncate=False)
+)drop("any").show(truncate=False)
